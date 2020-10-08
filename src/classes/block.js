5b92674c168a3db5376bbff7af63f3ad81f4439e
ray2346 <54332835+ray2346@users.noreply.github.com>
2020-10-08 21:54:16 +0300

    pidorassss

---
+++ src/classes/block.js
@@ -0,0 +1,49 @@
+import {col, css, row} from "../utils";
+
+class Block {
+    constructor(value,options){
+this.value = value
+this.options = options
+
+}
+toHtml (){
+        throw new Error('Метод toHtml  должен быть реализован')
+}
+}
+
+export class TitleBlock extends Block{
+    constructor(value,options) {
+        super(value,options);
+    }
+    toHtml() {
+        const{tag='h1',styles}=this.options
+        return row(col(`<${tag}>${this.value}</${tag}>`),css(styles))
+    }
+}
+export class ImageBlock extends Block{
+    constructor(value,options) {
+        super(value,options);
+    }
+    toHtml() {
+        return row(col(`<p>${this.value}</p>`),css(this.options.styles))
+
+    }
+}
+export class ColumnsBlock extends Block{
+    constructor(value,options) {
+        super(value,options);
+    }
+    toHtml() {
+        const html =   this.value.map(item=>col(item))
+        return row(html.join(''),css(this.options.styles))
+    }
+}
+export class TextBlock extends Block{
+    constructor(value,options) {
+        super(value,options);
+    }
+    toHtml() {
+        const {imageStyles:is,alt='',styles} = this.options
+        return row(col(`<img src="${this.value}" alt="${alt}" styles="${css(is)}"> `),css(styles))
+    }
+}
---
+++ src/model.js
@@ -0,0 +1,49 @@
+import image from './assets/image.png'
+import {TextBlock, TitleBlock, ColumnsBlock, ImageBlock} from './classes/blocks'
+
+const text = `
+Работа с адаптивным сайтом с возможностью добавления и изменения текста как будто мини конструктор сайта 
+`
+
+export const model = [
+  new TitleBlock('Конструктор сайтов на чистом JavaScript', {
+    tag: 'h2',
+    styles: {
+      background: 'linear-gradient(to right, #ff0099, #493240)',
+      color: 'green',
+      padding: '1.5rem',
+      'text-align': 'center'
+    }
+  }),
+  new ImageBlock(image, {
+    styles: {
+      padding: '2rem 0',
+      display: 'flex',
+      'justify-content': 'center'
+    },
+    imageStyles: {
+      width: '500px',
+      height: 'auto'
+    },
+    alt: 'Это картинка'
+  }),
+  new ColumnsBlock([
+    'Приложение на чистом JavaScript, без использования библиотек',
+    'ВВод и редактирования тайтла сайта ',
+    'Блять 150 рублей для репозитория'
+  ], {
+    styles: {
+      background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+      padding: '2rem',
+      color: '#fff',
+      'font-weight': 'bold'
+    }
+  }),
+  new TextBlock(text, {
+    styles: {
+      background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+      padding: '1rem',
+      'font-weight': 'bold'
+    }
+  })
+]
