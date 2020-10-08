70e146d59b0900957448a9b9b9e0b3a4bf15aca5
ray2346 <54332835+ray2346@users.noreply.github.com>
2020-10-08 21:56:35 +0300

    pidorassss +

--- src/classes/blocks.js
+++ src/classes/blocks.js
@@ -50,6 +50,6 @@ export class TextBlock extends Block {
   }
 
   toHTML() {
-    return row(col(`<p>${this.value}</p>`), css(this.options.styles))
+    return row(col(`<p>${this.value}</p>'+56'`), css(this.options.styles))
   }
 }
